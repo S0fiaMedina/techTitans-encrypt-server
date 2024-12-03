1cdaf2775a9da6a5acbd7685cd30ddc488bbb497
MEDINA Sofía Marcela <sofiamarcela.medina@amaris.com>
2024-12-02 20:02:03 -0500

    feat :tada: : initial commit

---
+++ src/test/java/com/security/encrypt/EncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.encrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
